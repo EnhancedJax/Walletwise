9910155f7fe6f5fda3b69c0d9e7cba97eafee5e7
Brandon <brandontang0325@gmail.com>
2024-02-04 10:25:10 +0000

    show toast on db error

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,20 +1,39 @@
 import { TablesInsert } from "../types/supabase";
 import { supabase } from "./supabaseInit";
+import Toast from "react-native-root-toast";
 
 export async function addAccount(account: TablesInsert<"accounts">) {
   const { data, error } = await supabase.from("accounts").insert(account);
-  if (error) console.error(error);
+  if (error) {
+    Toast.show(error.message, {
+      textColor: "#FF8888",
+    });
+    console.error(error);
+    return;
+  }
   return data;
 }
 
 export async function addCategory(category: TablesInsert<"categories">) {
   const { data, error } = await supabase.from("categories").insert(category);
-  if (error) console.error(error);
+  if (error) {
+    Toast.show(error.message, {
+      textColor: "#FF8888",
+    });
+    console.error(error);
+    return;
+  }
   return data;
 }
 
 export async function addEntry(entry: TablesInsert<"entries">) {
   const { data, error } = await supabase.from("entries").insert(entry);
-  if (error) console.error(error);
+  if (error) {
+    Toast.show(error.message, {
+      textColor: "#FF8888",
+    });
+    console.error(error);
+    return;
+  }
   return data;
 }
